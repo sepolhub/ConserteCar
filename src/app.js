d533222290feec07d745507d1fca0f3b25e6a264
sepolhub <nathanlcastro81@gmail.com>
2025-07-13 10:12:51 -0300

    criacao pagina de servicos

--- src/app.js
+++ src/app.js
@@ -4,30 +4,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configurar o Express para servir arquivos estáticos da pasta 'public'
-// Esta linha já está correta e é muito importante.
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Rota para a página inicial (index.html)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-// --- ROTAS ATUALIZADAS PARA LOGIN E CADASTRO ---
-
-// Rota para a página de login
 app.get('/login', (req, res) => {
-    // Em vez de enviar texto, agora envia o arquivo HTML
     res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
 });
 
-// Rota para a página de cadastro
 app.get('/cadastro', (req, res) => {
-    // Em vez de enviar texto, agora envia o arquivo HTML
     res.sendFile(path.join(__dirname, '..', 'public', 'cadastro.html'));
 });
 
-// VAI COMEÇAR O SERVIDOR AQUI!
+// --- NOVA ROTA PARA A PÁGINA DE SERVIÇOS ---
+app.get('/servicos', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'servicos.html'));
+});
+
+
 app.listen(PORT, () => {
     console.log(`Servidor ConserteCar rodando em http://localhost:${PORT}`);
     console.log('Pressione Ctrl+C para parar o servidor.');
